7a34412c08bb4595553383d65297352534a1e9ec
ilias-bikenov <ilias.bikenov@gmail.com>
2024-03-11 15:33:24 +0600

    Add nullish coalescing operator to config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV ?? 'development';
 
 const envFound = dotenv.config();
 if (envFound.error && process.env.NODE_ENV !== 'production') {
@@ -11,11 +11,11 @@ export default {
     port: process.env.PORT,
     app: {
         priceUpdateFrequency:
-            process.env.PRICE_UPDATE_FREQUENCY || '*/10 * * * * *',
-        commission: parseFloat(process.env.COMMISSION) || 0.01,
+            process.env.PRICE_UPDATE_FREQUENCY ?? '*/10 * * * * *',
+        commission: parseFloat(process.env.COMMISSION) ?? 0.01,
         binanceApiUrl: 'https://api.binance.com/api/v3',
     },
     logs: {
-        level: process.env.LOG_LEVEL || 'silly',
+        level: process.env.LOG_LEVEL ?? 'silly',
     },
 };
